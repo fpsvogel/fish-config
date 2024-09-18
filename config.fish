8a9eade0b3b4d367ba6fe9c88f8d0ec80bcf690f
Felipe Vogel <fps.vogel@gmail.com>
2024-09-18 15:46:19 -0400

    Add missing PATH paths

--- config.fish
+++ config.fish
@@ -13,6 +13,10 @@ fish_add_path ~/.local/bin
 fish_add_path /opt/homebrew/opt/cabal-install/bin
 fish_add_path /opt/homebrew/opt/haskell-language-server/bin
 
+fish_add_path ~/cod/roc_nightly-macos_apple_silicon-2024-09-09-9a4d556725
+
+fish_add_path ~/.nvm/versions/node/v18.19.1/bin
+
 fish_add_path ~/cod/ruby-advent-of-code
 
 fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
