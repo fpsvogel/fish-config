372d9ba52069ce22162717ce2f09b158c08bcad4
Felipe Vogel <fps.vogel@gmail.com>
2024-11-03 11:58:43 -0500

    Add direnv, and organize better

--- config.fish
+++ config.fish
@@ -2,24 +2,31 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
 end
 
-fish_add_path /opt/homebrew/bin
+source ~/.config/fish/alias
 
+# misc.
+fish_add_path /opt/homebrew/bin
+set -gx DIRENV_LOG_FORMAT ""
+direnv hook fish | source
+
+# Ruby
 fish_add_path ~/.rbenv/shims
 fish_add_path ~/.rbenv/bin
 
-# for Haskell
+# JS
+fish_add_path ~/.nvm/versions/node/v18.19.1/bin
+source ~/.deno/env.fish
+
+# Haskell
 fish_add_path /opt/homebrew/opt/llvm/bin
 fish_add_path ~/.local/bin
 fish_add_path /opt/homebrew/opt/cabal-install/bin
 fish_add_path /opt/homebrew/opt/haskell-language-server/bin
 
+# Roc
 fish_add_path ~/cod/roc_nightly-macos_apple_silicon-2024-09-09-9a4d556725
 
-fish_add_path ~/.nvm/versions/node/v18.19.1/bin
-
+# Coreutils
 fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
 # Copy commands here that should take precedence over coreutils (such as /bin/stty)
 fish_add_path ~/.bin
-
-source ~/.config/fish/alias
-source ~/.deno/env.fish
