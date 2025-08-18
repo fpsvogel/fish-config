0067e170f266eb280007deeb780e1b11b58a6b8e
Felipe Vogel <fps.vogel@gmail.com>
2025-08-18 10:57:44 -0400

    Add Datadog path

--- config.fish
+++ config.fish
@@ -4,11 +4,13 @@ end
 
 source ~/.config/fish/alias
 
-# misc.
+# Homebrew
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
-# set -gx DIRENV_LOG_FORMAT ""
+
+# Direnv
 direnv hook fish | source
+# set -gx DIRENV_LOG_FORMAT "" # not needed anymore
 
 # Ruby
 fish_add_path ~/.rbenv/shims
@@ -21,6 +23,9 @@ fish_add_path $nvm_data/v22.13.1/bin
 fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
 # Copy commands here that should take precedence over coreutils (such as /bin/stty)
 fish_add_path ~/.bin
+
+# Misc.
+fish_add_path ~/.local/pipx/venvs/datadog/bin
 
 export LEFTHOOK=0
 export EDITOR="code"
